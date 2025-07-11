c424babf9709640722db3f9e7b674adc6006637a
undefined <78364645+uint128-t@users.noreply.github.com>
2025-07-11 09:41:20 -0400

    example configuration

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # The name of the bot
-BOT_NAME= "defaultbot06"
+BOT_NAME= "bot"
 # Ollama model to use
-OLLAMA_AI_MODEL = "HammerAI/mythomax-l2"
+OLLAMA_AI_MODEL = "llama3.2:1b"
 # The system prompt for the bot
-SYSTEM_PROMPT = "You are a Discord bot named defaultbot06 that acts like a member of the server. Talk like an average internet user."
+SYSTEM_PROMPT = "You are a Discord bot named bot that acts like a member of the server. Talk like an average internet user."
 # How many messages to keep in the chat history
 MAX_MESSAGE_COUNT = 20
 # Reply ping when responding
